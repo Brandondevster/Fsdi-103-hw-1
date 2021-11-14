95eca0736493a64d5c531990e7295b44c3a2a296
Brandondevster <92768663+Brandondevster@users.noreply.github.com>
2021-11-13 23:29:52 -0800

    Add files via upload

---
+++ fsdi 103 hw 1/script.js
@@ -0,0 +1,23 @@
+let firstName = `Brandon`;
+let lastName = `Webster`
+let email = `Brandonkw13333@gmail.com`;
+let password = `password123`;
+let age = 28;
+let country = `United States`;
+let salaryMonthly = 4000;
+let salaryAnnually = salaryMonthly *12
+let jobTitle = `Software Developer`;
+let homeTown = `Boston`;
+let hobbies = `Boardsports and Videogames`;
+let favoriteColor = `Purple`;
+let pets = 2;
+let cat = 1;
+let dog = 1;
+let totalPets = cat + dog;
+let favoriteFood = `Japanese Ramen`
+let children = 0;
+let siblings = 4;
+let eyeColor = `Brown`;
+
+document.write(`First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nPassword: ${password}\n Age: ${age}\nCountry: ${country}\nSalary Monthly: ${salaryMonthly}\nSalary Annually: ${salaryAnnually}/nJob Title: ${jobTitle}\nHome Town: ${homeTown}\nHobbies : ${hobbies}\nFavorite Color: ${favoriteColor}/nPets: ${pets}\nCat: ${cat}\nDog: ${dog}\nTotal Pets: ${totalPets}/nFavorite Food: ${favoriteFood}\nChildren: ${children}\nSiblings: ${siblings}\nEye Color: ${eyeColor}`)
+
